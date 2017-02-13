0feaa82461c453cdd327c1b798069baecbf13863
valrigori <valentin.rigori@epfl.ch>
2017-02-13 17:28:57 +0100

    add implemented

--- NavBarTest/app/src/main/java/com/example/navbartest/AddFragment.java
+++ NavBarTest/app/src/main/java/com/example/navbartest/AddFragment.java
@@ -1,14 +1,29 @@
 package com.example.navbartest;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class AddFragment extends Fragment {
+
+    private Button datePick;
+    private EditText price;
+    private EditText job_description;
+    private Button add_service;
+    private static EditText date;
 
     public AddFragment() {
 
@@ -17,6 +32,53 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        datePick = (Button) view.findViewById(R.id.date_pick);
+        price = (EditText) view.findViewById(R.id.price);
+        job_description = (EditText) view.findViewById(R.id.job_description);
+        add_service = (Button) view.findViewById(R.id.add_service);
+        date = (EditText) view.findViewById(R.id.date);
+
+        setDatePick();
+
         return view;
+    }
+
+    private void setDatePick() {
+        datePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getFragmentManager(), "timePicker");
+            }
+        });
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            date.setText((day + "/" + (month + 1) + "/" + year).toString());
+        }
+    }
+
+    private void add_service() {
+        add_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
