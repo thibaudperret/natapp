e3a0d1fb0996d8ea13a70439ae8bfa4e31601f1e
thibaudperret <thibaud.perret@epfl.ch>
2017-02-17 15:23:43 +0100

    profile fragment done

--- NavBarTest/app/src/main/java/com/example/navbartest/ProfileFragment.java
+++ NavBarTest/app/src/main/java/com/example/navbartest/ProfileFragment.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ToggleButton;
 
 public class ProfileFragment extends Fragment{
+
+    private ToggleButton seeHideButton;
+    private LinearLayout commentsLayout;
 
     public ProfileFragment() {
 
@@ -17,6 +23,25 @@ public class ProfileFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         MainActivity m = (MainActivity) getActivity();
         m.setTitle("Profile");
+
+        seeHideButton = (ToggleButton) view.findViewById(R.id.see_hide_toggle);
+        commentsLayout = (LinearLayout) view.findViewById(R.id.user_comments);
+
+        setToggleListener();
+
         return view;
+    }
+
+    private void setToggleListener() {
+        seeHideButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    commentsLayout.setVisibility(View.VISIBLE);
+                } else {
+                    commentsLayout.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
